ff1cc936c16963e504a806f19e6197fab5af2b75
phateje <phateje@gmail.com>
2024-09-11 18:47:56 -0700

    can move and restack. Fix bug where an empty stack has no clickable portion

--- src/cardMoveHandler.ts
+++ src/cardMoveHandler.ts
@@ -1,6 +1,9 @@
+const { Card } = require("./card.ts");
+const { Stack } = require("./stack.ts");
+
 exports.CardMoveHandler = class CardMoveHandler {
   private selectedCard: any | undefined;
-  private previousStack: any | undefined;
+  private sourceStack: any | undefined;
   private gameContainer;
 
   constructor(gameContainer: HTMLDivElement) {
@@ -20,22 +23,41 @@ exports.CardMoveHandler = class CardMoveHandler {
 
   private stackClick(event: any) {
     const { stack, card, clickEvent } = event.detail;
-    if (card == stack.peekTopCard()) {
-      // clear previous card
-      if (this.selectedCard && this.previousStack && stack != this.previousStack) {
-        console.log("should restore card");
-        this.selectedCard.getDomElement().style.position = "";
-        this.previousStack.stackTopCard(this.selectedCard);
-      }
 
-      stack.removeTopCard();
-      this.previousStack = stack;
-      this.selectedCard = card;
-      const cardDomElement = card.getDomElement();
-      cardDomElement.style.position = "fixed";
-      this.setCardPosition(clickEvent);
-      this.gameContainer.appendChild(cardDomElement);
+    const isNewClick = card == stack.peekTopCard() && !this.selectedCard;
+    const isClickOnDestinationStack = this.selectedCard && stack.size() < 3;
+
+    console.log("so what's going on here", {
+      stack,
+      card,
+      selectedCard: this.selectedCard,
+      sourceStack: this.sourceStack,
+      isNewClick,
+      isClickOnDestinationStack,
+    });
+    if (isNewClick) {
+      this.selectNewCard(card, stack, clickEvent);
+    } else if (isClickOnDestinationStack) {
+      this.stackCardOn(stack);
     }
+  }
+
+  private selectNewCard(card: typeof Card, stack: typeof Stack, clickEvent: any) {
+    stack.removeTopCard();
+    this.sourceStack = stack;
+    this.selectedCard = card;
+    const cardDomElement = card.getDomElement();
+    cardDomElement.style.position = "fixed";
+    this.setCardPosition(clickEvent);
+    this.gameContainer.appendChild(cardDomElement);
+  }
+
+  private stackCardOn(stack: typeof Stack) {
+    const cardDomElement = this.selectedCard.getDomElement();
+    cardDomElement.style.position = "";
+    stack.stackTopCard(this.selectedCard);
+    this.sourceStack = undefined;
+    this.selectedCard = undefined;
   }
 
   public getHandlers() {
